da756498caf60326851373d6d7a41a1223df874a
Michael Yarberry <yarberrymr@gmail.com>
2022-11-14 15:52:02 -0600

    all requested changes made to reflect business owners needs

--- src/Body.js
+++ src/Body.js
@@ -2,9 +2,14 @@ import React from "react";
 import SkidSteer from "./images/SkidSteer.png";
 import Attch1 from "./images/Attch1.png";
 import Attch2 from "./images/Attch2.png";
+import Base from "./images/Base.jpg";
+import Base2 from "./images/Base2.jpg";
+import Bucket from "./images/Bucket.jpg";
+import Demo from "./images/Demo.jpg";
+import Shred from "./images/Shred.jpg";
 
 export default function body() {
-  return (
+  return (<div>
     <div className="row d-flex justify-content-center body-div">
       <div className=" description-card card text-center">
         <div className="card-header">What do we do?</div>
@@ -15,14 +20,111 @@ export default function body() {
             <li>Lot Leveling</li>
             <li>Underbrush Shredding</li>
             <li>Base Driveway Repair</li>
-            <li>And More...</li>
+            <li>And More!!!</li>
           </ul>
         </div>
       </div>
-
+      </div>
+      <div className="row d-flex justify-content-center body-div">
+      <div
+        id="carouselExampleCaptions1"
+        className="col-6 carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions1"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions1"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions1"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions1"
+            data-bs-slide-to="3"
+            aria-label="Slide 4"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={SkidSteer} className="d-block" alt="SkidSteer with attachments" />
+            <div className="carousel-caption d-none d-md-block">
+              <h5 className="transparent-bg">Skid Steer</h5>
+              <p className="transparent-bg">
+                Has multiple attachments that can be used for various work.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={Attch1} className="d-block " alt="SkidSteer shredder attachment"></img>
+            <div className="carousel-caption d-none d-md-block">
+              <h5 className="transparent-bg">Shredder Attachment</h5>
+              <p className="transparent-bg">
+                This can shred up to 2"-3" of grass or underbrush.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={Attch2} className="d-block " alt="SkidSteer rock grapple bucket"></img>
+            <div className="carousel-caption d-none d-md-block">
+              <h5 className="transparent-bg">Rock Grapple Bucket</h5>
+              <p className="transparent-bg">
+                This can carry cut brush, helps in demo clean up, and sifts rock and dirt.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={Bucket} className="d-block " alt="SkidSteer large bucket"></img>
+            <div className="carousel-caption d-none d-md-block">
+              <h5 className="transparent-bg">Skid Steer Bucket</h5>
+              <p className="transparent-bg">
+                Large skid steer bucket with teeth welded to bottom (not pictured) for easy clean up of land.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions1"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions1"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
       <div
         id="carouselExampleCaptions"
-        className="carousel slide"
+        className="col-6 carousel slide"
         data-bs-ride="carousel"
       >
         <div className="carousel-indicators">
@@ -46,32 +148,47 @@ export default function body() {
             data-bs-slide-to="2"
             aria-label="Slide 3"
           ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="3"
+            aria-label="Slide 4"
+          ></button>
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src={SkidSteer} className="d-block w-50" alt="..." />
+            <img src={Demo} className="d-block " alt="Lot cleared after demolition" />
             <div className="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
+              <h5 className="transparent-bg">After house demo</h5>
+              <p className="transparent-bg">
+                Lot cleared by skid steer after house demolition.
               </p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src={Attch1} className="d-block w-50" alt="..."></img>
+            <img src={Base} className="d-block " alt="spillway cleanout"></img>
             <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
+              <h5 className="transparent-bg">Spillway cleanout #1</h5>
+              <p className="transparent-bg">
+                Cleaned out spillway of tank and dug down deeper with skid steer.
               </p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src={Attch2} className="d-block w-50" alt="..."></img>
+            <img src={Base2} className="d-block " alt="spillway cleanout"></img>
             <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
+              <h5 className="transparent-bg">Spillway cleanout #2</h5>
+              <p className="transparent-bg">
+              Cleaned out spillway of tank and dug down deeper with skid steer.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={Shred} className="d-block " alt="Lot shredding after"></img>
+            <div className="carousel-caption d-none d-md-block">
+              <h5 className="transparent-bg">Lot shredding</h5>
+              <p className="transparent-bg">
+              After picture of lot shredded with skid steer and shredding attachment.
               </p>
             </div>
           </div>
@@ -101,6 +218,7 @@ export default function body() {
           <span className="visually-hidden">Next</span>
         </button>
       </div>
+    </div>
     </div>
   );
 }
--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function contact() {
   return (
-    <div className="row d-flex justify-content-center">
+    <div id="contact" className="row d-flex justify-content-center">
         <hr className="contact-header"></hr>
         <h2>Contact us for a free quote!</h2>
       <div className="row d-flex justify-content-center">
@@ -12,7 +12,8 @@ export default function contact() {
             <p className="card-text">
               Lloyd owns and operates the equipment.
             </p>
-            <a href="tel:+1-254-291-6752" className="btn btn-primary">Call Lloyd</a>
+            <a href="tel:+1-254-449-1362" className="btn btn-primary">Click to Call Lloyd</a>
+            <p>(254)-449-1362</p>
           </div>
         </div>
         <div className="contact-card card text-center">
@@ -21,20 +22,22 @@ export default function contact() {
             <p className="card-text">
               Can't get ahold of Lloyd, try calling the office!
             </p>
-            <a href="tel:+1-254-291-6752" className="btn btn-primary">Call Melanie</a>
+            <a href="tel:+1-254-449-1324" className="btn btn-primary">Click to Call Office</a>
+            <p>(254)-449-1324</p>
           </div>
         </div>
       </div>
       <div className="row d-flex justify-content-center">
-        <div className="contact-card card text-center">
+        <div className="contact-card card text-center email-card">
           <div className="card-body">
             <h5 className="card-title">Business Email</h5>
             <p className="card-text">
               Emails go straight to the business and will be responded to asap.
             </p>
-            <a href="mailto:EMAILADDRESS" className="btn btn-primary">
-              Go somewhere
+            <a href="mailto:doublehlandmaintenance@gmail.com" className="btn btn-primary">
+              Send an Email
             </a>
+            <p>doublehlandmaintenance@gmail.com</p>
           </div>
         </div>
       </div>
