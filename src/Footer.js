3ee77dc1448c0b406aabda31ce99a79b91f7390b
Michael Yarberry <yarberrymr@gmail.com>
2024-04-12 16:19:43 -0500

    add footer file

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function footer() {
+  return (
+    <div class="footer-div">
+      <p>Copyright: Double H Land Maintenance and More 2022</p>
+    </div>
+  );
+}
