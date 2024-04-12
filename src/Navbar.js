c528cfd3cf3ddb005f3360060df49c5172862de6
Michael Yarberry <yarberrymr@gmail.com>
2024-04-12 16:19:28 -0500

    Styling changes and phone number change

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function navbar() {
   return (
     <div>
-      <h2>Double H Land Maintenance and More</h2>
-      <button onClick={() => window.location.replace("/#contact")} type="button" className="btn btn-link">
+      <h2 class="title-header">Double H Land Maintenance and More</h2>
+      <button onClick={() => window.location.replace("/#contact")} type="button" className="btn btn-link contact-btn">
         Contact Us
       </button>
     </div>
